72c663a5af6946ba23307c79958df1bb800f35da
Carter <cmeado11@charlotte.edu>
2026-04-14 18:25:41 -0400

    Updated /tasks endpoints to accept completed query

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,7 +1,17 @@
 import * as taskRepository from '../repositories/taskRepo.js';
 
-export async function getAllTasks() {
-  return taskRepository.findAll();
+export async function getAllTasks(completed) {
+  let completedFilter;
+
+  if (completed === 'true') {
+    completedFilter = true;
+  } else if (completed === 'false') {
+    completedFilter = false;
+  } else {
+    completedFilter = undefined;
+  }
+
+  return taskRepository.findAll(completedFilter);
 }
 
 export async function createTask(newTask) {
